17e983b07194a60b19e1c9bf2323cb2fbade3a7d
Augustas <dragaz123@gmail.com>
2022-09-12 17:32:59 +0300

    First

---
+++ RsignalTest/Hubs/Chat.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace RsignalTest.Hubs
+{
+    public class Chat : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
